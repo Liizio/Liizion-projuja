6ecdcb4298207f8bb42b0d0cbb62ccfb34fce614
Liizio <pauli.pippola@gmail.com>
2016-02-09 15:41:25 +0200

    jij

---
+++ HelloGitHubApplication/D5t4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D5t4
+{
+    class Program
+    {
+        abstract class Engine
+        {
+            public int Size;
+            public int Cylinders;
+            public int power;
+            public int rpm;
+            public string fuel;
+        }
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
